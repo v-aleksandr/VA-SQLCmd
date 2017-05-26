e06376a00512ee21b8deeb1aecf1ee91b8be7108
Александр <voskolup.aleksandr@gmail.com>
2017-05-26 18:02:54 +0300

    Тестируем Find

--- test/ua.com.juja.sqlcmd/controller/command/FindTest.java
+++ test/ua.com.juja.sqlcmd/controller/command/FindTest.java
@@ -20,16 +20,17 @@ public class FindTest {
 
     private DatabaseManager manager;
     private View view;
+    private Command command;
 
     @Before
     public void setup() {
         manager = mock(DatabaseManager.class);
         view = mock(View.class);
+        command = new Find(manager, view);
     }
 
     @Test
     public void testPrintTableData() {
-        Command command = new Find(manager, view);
         when(manager.getTableColumns("user")).thenReturn(new String[] {"id", "name", "password"});
         DataSet user1 = new DataSet();
         user1.put("id",12);
@@ -43,20 +44,16 @@ public class FindTest {
         when(manager.getTableData("user")).thenReturn(data);
         command.process("find|user");
 
-        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-        verify(view, atLeastOnce()).write(captor.capture());
-        assertEquals("[----------------------------, " +
+        shouldPrint("[----------------------------, " +
                 "|id|name|password|, " +
                 "----------------------------, " +
                 "|12|Stiven|******|, " +
                 "|13|Eva|++++++|, " +
-                "----------------------------]", captor.getAllValues().toString());
+                "----------------------------]");
     }
 
     @Test
     public void testCanProcessFindWithParametersString() {
-        Command command = new Find(manager, view);
-
         boolean canProcess = command.canProcess("find|user");
 
         assertTrue(canProcess);
@@ -66,41 +63,52 @@ public class FindTest {
 
     @Test
     public void testCanProcessFindWithoutParametersString() {
-        Command command = new Find(manager, view);
-
         boolean canProcess = command.canProcess("find");
-
         assertFalse(canProcess);
-
-
     }
 
     @Test
 
     public void testCanProcessQweString() {
-        Command command = new Find(manager, view);
-
         boolean canProcess = command.canProcess("qwe|user");
-
         assertFalse(canProcess);
-
-
     }
 
     @Test
     public void testPrintEmptyTableData() {
-        Command command = new Find(manager, view);
         when(manager.getTableColumns("user")).thenReturn(new String[] {"id", "name", "password"});
 
-        DataSet[] data = new DataSet[0];
-        when(manager.getTableData("user")).thenReturn(data);
+        when(manager.getTableData("user")).thenReturn(new DataSet[0]);
         command.process("find|user");
 
-        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-        verify(view, atLeastOnce()).write(captor.capture());
-        assertEquals("[----------------------------, " +
+        shouldPrint("[----------------------------, " +
                 "|id|name|password|, " +
                 "----------------------------, " +
-                "----------------------------]", captor.getAllValues().toString());
+                "----------------------------]");
+    }
+
+    private void shouldPrint(String expected) {
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(view, atLeastOnce()).write(captor.capture());
+        assertEquals(expected, captor.getAllValues().toString());
+    }
+
+    @Test
+    public void testPrintTableDataWithOneColumn() {
+        when(manager.getTableColumns("test")).thenReturn(new String[] {"id"});
+        DataSet user1 = new DataSet();
+        user1.put("id",12);
+        DataSet user2 = new DataSet();
+        user2.put("id",13);
+        DataSet[] data = new DataSet[] {user1, user2};
+        when(manager.getTableData("test")).thenReturn(data);
+        command.process("find|test");
+
+        shouldPrint("[----------------------------, " +
+                "|id|, " +
+                "----------------------------, " +
+                "|12|, " +
+                "|13|, " +
+                "----------------------------]");
     }
 }
